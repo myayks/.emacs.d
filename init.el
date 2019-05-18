0cf03bc35750e50c51367618367dd48d21426d19
Maksims Mironovs <mmironovs@Maksimss-MacBook-Pro.local>
2019-05-18 01:00:00 +0100

    First settings from my mac

---
+++ init.el
@@ -0,0 +1,21 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme `cyberpunk-2019 t)
+
+'(mode-line ((t (:foreground "#fafafa" :background "DarkOrange" :box nil))))
+'(mode-line-inactive ((t (:foreground "#fafafa" :background "#666666" :box nil))))
+
+;;(set-face-attribute 'flycheck-error nil :underline '(:color "#FF4081"))
+;;(set-face-attribute 'flycheck-warning nil :underline '(:color "#FF9C00"))
+;;(set-face-attribute 'flycheck-info nil :underline '(:color "#9C00FF"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
